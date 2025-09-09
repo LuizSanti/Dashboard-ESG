d527a59e4751dcc28c584fd9092fde4294166235
Luiz Gabriel <lgabrielsantiago09@gmail.com>
2025-09-09 13:41:41 -0300

    html e css base prontos e puxando apis de desamatamento de cada bioma

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+async function carregarDesmatamentoBrasil() {
+    const urls = [
+        "https://terrabrasilis.dpi.inpe.br/geoserver/prodes-pantanal-nb/accumulated_deforestation_2000/ows",
+        "https://terrabrasilis.dpi.inpe.br/geoserver/prodes-amazon-nb/accumulated_deforestation_2007_biome/ows",
+        "https://terrabrasilis.dpi.inpe.br/geoserver/prodes-cerrado-nb/accumulated_deforestation_2000/ows",
+        "https://terrabrasilis.dpi.inpe.br/geoserver/prodes-pampa-nb/accumulated_deforestation_2000/ows",
+        "https://terrabrasilis.dpi.inpe.br/geoserver/prodes-legal-amz/accumulated_deforestation_2007/ows",
+        "https://terrabrasilis.dpi.inpe.br/geoserver/prodes-caatinga-nb/accumulated_deforestation_2000/ows",
+        "https://terrabrasilis.dpi.inpe.br/geoserver/prodes-mata-atlantica-nb/accumulated_deforestation_2000/ows"
+    ];
+}
